a56c411c011bfcec091960c7eb7b6d2981ec6d0b
Michael Lynch <mplynch@gmail.com>
2015-08-07 00:09:41 -0500

    Moved BPM to subfolder and added suggestionbox

---
+++ suggestionbox/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ui.router',
+  'myApp.list',
+  'myApp.new',
+  'myApp.welcome',
+  'myApp.version',
+  'myApp.datastub'
+]).
+
+// TODO: Replace ng-router with ui-router.  See http://www.funnyant.com/angularjs-ui-router/
+// The bit about state machines is what we need...
+
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+}]);
